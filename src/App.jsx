39a86882a862912c64f64ca05304a839f461289a
Foram patel <foramp0909@gmail.com>
2025-01-18 17:46:03 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Registration from './Component/Registration'
+
+function App() {
+  return (
+    <div>
+      <Registration/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/Registration.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Registration() {
+  return (
+    <div className='flex h-full w-screen bg-black '>
+        hello
+    </div>
+  )
+}
+
+export default Registration
